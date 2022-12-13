a9e6d4b25126afc772ec04a216134deae933cd60
theblocdoc <theblocdoc@gmail.com>
2022-12-13 17:05:44 -0500

    add pfp, modify css, modify app.js, modify signup

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require("express");
+const request = require("request");
 const bodyParser = require("body-parser");
 const https = require("https");
+const { urlencoded } = require("body-parser");
 const port = 3000;
 
 const app = express();
+app.use(urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(__dirname + "/signup.html");
+  console.log("Everything should be working at this point");
 });
 
 app.listen(port, function () {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}...`);
 });
